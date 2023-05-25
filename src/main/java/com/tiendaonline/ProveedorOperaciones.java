f6dfcd6e077daa33f7451c49e5acc11756c60ff1
Francisco <franciscoizadaw@gmail.com>
2023-05-25 16:21:00 +0200

    Mi primmer commit(Francisco Iza)

---
+++ src/main/java/com/tiendaonline/ProveedorOperaciones.java
@@ -0,0 +1,17 @@
+package com.tiendaonline;
+
+public class Proveedor{
+	private int cif;
+	private String nombre_empresa;
+	private int telefono;
+	private String direccion;
+	private String correo;
+	
+	
+	 public Proveedor() { }
+	 
+
+	
+	
+	
+}
