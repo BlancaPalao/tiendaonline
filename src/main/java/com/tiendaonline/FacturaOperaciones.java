a7c34b748200901aa40d41f372e23a4de925a9f7
markakop <markis2802@gmail.com>
2023-05-25 16:20:44 +0200

    Commit FacturaOperaciones

---
+++ src/main/java/com/tiendaonline/FacturaOperaciones.java
@@ -0,0 +1,17 @@
+package com.tiendaonline;
+
+import java.util.ArrayList;
+
+public class FacturaOperaciones {
+
+    public boolean actualizarFactura(int codigo, ArrayList<Integer> codigos, String cambios) {
+
+        for (Integer i : codigos)
+            if (i == codigo) {
+                cambios = "UPDATE factura SET cambios WHERE id_factura = " + cambios;
+                System.out.println(cambios);
+                return true;
+            }
+        return false;
+    }
+}
