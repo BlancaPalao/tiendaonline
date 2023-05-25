e6e023466100ad277d71aa45cfc140b3e2e77126
Usuario <Usuario@10.68.10.176>
2023-05-25 16:58:01 +0200

    Commit con clases que quería meter

---
+++ src/main/java/com/tiendaonline/ProductoDTO.java
@@ -0,0 +1,51 @@
+package com.tiendaonline;
+
+public class ProductoDTO {
+    private int id;
+    private String nombre;
+    private String descripcion;
+    private double precio;
+    // Otros atributos que representen las columnas de la tabla Producto en la base de datos
+
+    // Constructor
+    public ProductoDTO(int id, String nombre, String descripcion, double precio) {
+        this.id = id;
+        this.nombre = nombre;
+        this.descripcion = descripcion;
+        this.precio = precio;
+    }
+
+    // Getters y setters para los atributos
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+}
