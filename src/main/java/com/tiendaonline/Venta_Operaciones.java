439d3b3364fb4749755b42e2b7c62b9ab69b9551
alumno <alumno@daw1-15>
2023-05-25 16:08:35 +0200

    alta_de_venta(25 may)

---
+++ src/main/java/com/tiendaonline/Venta_Operaciones.java
@@ -0,0 +1,36 @@
+package com.tiendaonline;
+
+import java.util.Scanner;
+
+/**
+ * Hello world!
+ *
+ */
+public class Venta_Operaciones 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Push realizado desde el Visual Studio Code" );
+        
+        /*Crear una clase DTO para encapsular 
+         * la tabla Venta de BBDD y crea una 
+         * clase con el nombre VentaOperaciones.java con el método 
+         * para dar de alta venta en la tienda online (permitir buscar 
+         * N productos asociados a una Venta por códigos o por fecha así 
+         * como vincular cada venta a un cliente concreto).
+         * */
+        
+         
+         Scanner sc= new Scanner(System.in);
+         String cliente;
+         String cdo_producto;
+         
+         
+    }
+    
+    public boolean alta_venta() {
+    	return true;
+    	
+    }
+    
+}
